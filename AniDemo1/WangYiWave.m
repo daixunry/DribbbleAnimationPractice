0c1f8ebce833ccebd836e7048d1b3728bf3f1678
daixu <624918137@qq.com>
2016-01-16 15:37:02 +0800

    优化wangyiwave动画

--- AniDemo1/WangYiWave.m
+++ AniDemo1/WangYiWave.m
@@ -50,50 +50,6 @@
     [self.layer addSublayer:_layer2];
 }
 
-- (void)drawRect:(CGRect)rect
-{
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(context, [UIColor whiteColor].CGColor);
-
-    CGMutablePathRef pathRef = CGPathCreateMutable();
-    CGFloat startY = _waveHeight*sinf(_offset*M_PI/_waveWidth);
-    CGPathMoveToPoint(pathRef, NULL, 0, startY);
-    for (CGFloat i = 0.0; i < _waveWidth; i ++) {
-        CGFloat y = 1.1*_waveHeight*sinf(2.5*M_PI*i/_waveWidth + _offset*M_PI/_waveWidth) + _h;
-        CGPathAddLineToPoint(pathRef, NULL, i, y);
-    }
-    CGPathAddLineToPoint(pathRef, NULL, _waveWidth, 40);
-    CGPathAddLineToPoint(pathRef, NULL, 0, 40);
-    
-    UIBezierPath *_path = [UIBezierPath bezierPath];
-    _path.CGPath = pathRef;
-    [_path closePath];
-    
-    _layer.path  =_path.CGPath;
-    _layer.fillColor = [UIColor lightGrayColor].CGColor;
-    CGPathRelease(pathRef);
-    
-    
-    CGMutablePathRef pathRef2 = CGPathCreateMutable();
-    CGFloat startY2 = _waveHeight*sinf(_offset*M_PI/_waveWidth + M_PI/4);
-    CGPathMoveToPoint(pathRef2, NULL, 0, startY2);
-    for (CGFloat i = 0.0; i < _waveWidth; i ++) {
-        CGFloat y = _waveHeight*sinf(2.5*M_PI*i/_waveWidth + 3*_offset*M_PI/_waveWidth + M_PI/4) + _h;
-        CGPathAddLineToPoint(pathRef2, NULL, i, y);
-    }
-    CGPathAddLineToPoint(pathRef2, NULL, _waveWidth, 40);
-    CGPathAddLineToPoint(pathRef2, NULL, 0, 40);
-    
-    UIBezierPath *_path2 = [UIBezierPath bezierPath];
-    _path2.CGPath = pathRef2;
-    [_path2 closePath];
-    
-    _layer2.path  =_path2.CGPath;
-    _layer2.fillColor = [UIColor lightGrayColor].CGColor;
-    CGPathRelease(pathRef2);
-
-}
-
 - (void)wave
 {
     _link = [CADisplayLink displayLinkWithTarget:self selector:@selector(doAni)];
@@ -109,7 +65,36 @@
 - (void)doAni
 {
     _offset += _speed;
-    [self setNeedsDisplay];
+    
+    CGMutablePathRef pathRef = CGPathCreateMutable();
+    CGFloat startY = _waveHeight*sinf(_offset*M_PI/_waveWidth);
+    CGPathMoveToPoint(pathRef, NULL, 0, startY);
+    for (CGFloat i = 0.0; i < _waveWidth; i ++) {
+        CGFloat y = 1.1*_waveHeight*sinf(2.5*M_PI*i/_waveWidth + _offset*M_PI/_waveWidth) + _h;
+        CGPathAddLineToPoint(pathRef, NULL, i, y);
+    }
+    CGPathAddLineToPoint(pathRef, NULL, _waveWidth, 40);
+    CGPathAddLineToPoint(pathRef, NULL, 0, 40);
+    CGPathCloseSubpath(pathRef);
+    
+    _layer.path = pathRef;
+    _layer.fillColor = [UIColor lightGrayColor].CGColor;
+    CGPathRelease(pathRef);
+    
+    CGMutablePathRef pathRef2 = CGPathCreateMutable();
+    CGFloat startY2 = _waveHeight*sinf(_offset*M_PI/_waveWidth + M_PI/4);
+    CGPathMoveToPoint(pathRef2, NULL, 0, startY2);
+    for (CGFloat i = 0.0; i < _waveWidth; i ++) {
+        CGFloat y = _waveHeight*sinf(2.5*M_PI*i/_waveWidth + 3*_offset*M_PI/_waveWidth + M_PI/4) + _h;
+        CGPathAddLineToPoint(pathRef2, NULL, i, y);
+    }
+    CGPathAddLineToPoint(pathRef2, NULL, _waveWidth, 40);
+    CGPathAddLineToPoint(pathRef2, NULL, 0, 40);
+    CGPathCloseSubpath(pathRef2);
+    
+    _layer2.path = pathRef2;
+    _layer2.fillColor = [UIColor lightGrayColor].CGColor;
+    CGPathRelease(pathRef2);
 }
 
 
